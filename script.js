302bed3e3151cc47ff925fc4212f158b20f4ded8
RanjithaElaiyaperumal <ranjitharanjitha302003@gmail.com>
2024-11-22 13:00:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navbar links
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector(link.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
